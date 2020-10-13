f64c3039df44c49a6abc6722d5e850625bc50eee
D'Angelo Minnis <dangelominnis@gmail.com>
2020-10-13 10:24:43 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// hamburger menu
+const nav = document.getElementById("nav__mobile-ul");
+const toggle = document.getElementById("toggle");
+
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
